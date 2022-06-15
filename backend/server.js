1091d0ee2ed7ae127b32c4285e075af0a610bcfc
Rj <rjimaw7777@gmail.com>
2022-06-15 21:23:05 +0800

    Deploying to heroku

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,21 @@ app.use("/api/users", require("./routes/userRoutes"));
 // Employees Routes
 app.use("/api/employees", require("./routes/employeesRoutes"));
 
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to my Coding Exam" });
+  });
+}
+
 // Error middleware
 app.use(errorHandler);
 
